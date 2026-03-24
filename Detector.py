959fa01b5171b9f11d0f5410211d0f0e93e54f76
MatKa <m.kandalintsev@student.maastrichtuniversity.nl>
2026-03-24 12:21:15 +0100

    release for data collection

--- Detector.py
+++ Detector.py
@@ -20,19 +20,24 @@ class Detector:
         for i in range (1, len(self.keys)):
             gap= self.keys[i][1] - self.keys[i-1][1]
             self.keysWithGaps.append((self.keys[i-1][0], gap))
-        print("Analysing", len(self.keysWithGaps), "keys:", self.keysWithGaps)
-        if len(self.keysWithGaps)>=10:
-            self.blocked=True
-            self.Lockscreen.block()
+
+        # TODO remove ts for data collection
+        #print("Analysing", len(self.keysWithGaps), "keys:", self.keysWithGaps)
+        # if len(self.keysWithGaps)>=10:
+        #     self.blocked=True
+        #     self.Lockscreen.block()
+
+
         #output to csv
         key_count, avg_gap, var = self.Format(self.keysWithGaps)
         self.DataCollector.save(key_count, avg_gap, var)
     def Format(self,keysWithGaps):
+
+        if len(keysWithGaps) == 0:  return 0, 0, 0
+
         gaps = []
         for key, gap in self.keysWithGaps:
             gaps.append(gap)
-        if len(keysWithGaps) == 0:
-            return 0, 0, 0
         key_count = len(keysWithGaps)
         avg_gap = statistics.mean(gaps)
         var = np.var(gaps)
